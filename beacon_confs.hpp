230fb005a57115bfdf03ad73d955e96b8eb2ef41
Andorlatch <96745233+Andorlatch@users.noreply.github.com>
2023-03-06 10:20:54 +0300

    Esrak3172-URR-V1.0
    URR means (UPLINK RESPONSE RETURN). But, I must check code again cus its kind of ill-formed.

---
+++ beacon_confs.hpp
@@ -0,0 +1,70 @@
+#ifndef BEACON_CONFS_H
+#define BEACON_CONFS_H
+
+//To set debug mode
+#define DEBUG
+
+
+////P2P
+//<Freq>,<SF>,<Bandwidth>,<CR>,<Preamble>,<Power>
+//AT+P2P=868000000:9:125:0:8:15
+#define P2P_FREQ      "868000000"
+#define P2P_SF        "7"
+#define P2P_BANDWITH  "125"
+#define P2P_CR        "0"
+#define P2P_PREAMBLE  "8"
+#define P2P_POWER     "15"
+//#define BAND
+#define P2P_OPS P2P_FREQ":"P2P_SF":"P2P_BANDWITH":"P2P_CR":"P2P_PREAMBLE":"P2P_POWER
+//Programmer needs to configure lora operation confs here
+#define JOIN_REQ "1:0:10:8" // <Join command><Auto-Join config><Reattempt interval><numbers of join attempts>
+//NWM
+#define P2P     "0"
+#define LORAWAN "1"
+
+
+////LORAWAN
+//CLASS
+#define CLASS_A "A"
+#define CLASS_B "B"
+#define CLASS_C "C"
+//BAND
+#define BAND    "4"
+//NJM
+#define ABP     "0"
+#define OTAA    "1"
+
+//Programmer needs to configure lora join keys here
+
+#define DEV_EUI   "68cbd47450d82f3d"
+#define DEV_ADDR  "01c94f60"
+#define APPS_KEY  "1f264b78a764570f53d900f3d435cbce"
+#define NWKS_KEY  "1f264b78a764570f53d900f3d435cbce"
+#define APP_KEY
+
+//Programmer needs to set conf parameters here
+#define NWM   LORAWAN
+#define NJM   ABP
+#define CLASS CLASS_A
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
